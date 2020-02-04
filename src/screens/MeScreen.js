d072bd5dead6125c910f853d0a8044deb1854e22
tony.yang <tony.yang@bwoil.com>
2020-02-04 18:02:38 +0800

    add JPush

--- src/screens/MeScreen.js
+++ src/screens/MeScreen.js
@@ -1,12 +1,77 @@
 import React ,{Component} from 'react';
-import { StyleSheet,View,Text} from 'react-native';
+import { StyleSheet,View,Text,TouchableHighlight} from 'react-native';
+import JPush from 'jpush-react-native';
+
+
+
+class Button extends React.Component {
+    render() {
+        return <TouchableHighlight
+            onPress={this.props.onPress}
+            underlayColor='#e4083f'
+            activeOpacity={0.5}
+        >
+            <View
+                style={styles.setBtnStyle}>
+                <Text
+                    style={styles.textStyle}>
+                    {this.props.title}
+                </Text>
+            </View>
+        </TouchableHighlight>
+    }
+}
+
 
 export default class MeScreen extends Component {
+    constructor(props) {
+        super(props);
+    }
+    componentDidMount() {
+        JPush.init();
+        //连接状态
+        this.connectListener = result => {
+            console.log("connectListener:" + JSON.stringify(result))
+        };
+        JPush.addConnectEventListener(this.connectListener);
+        //通知回调
+        this.notificationListener = result => {
+            console.log("notificationListener:" + JSON.stringify(result))
+        };
+        JPush.addNotificationListener(this.notificationListener);
+        //本地通知回调
+        this.localNotificationListener = result => {
+            console.log("localNotificationListener:" + JSON.stringify(result))
+        };
+        JPush.addLocalNotificationListener(this.localNotificationListener);
+        //自定义消息回调
+        this.customMessageListener = result => {
+            console.log("customMessageListener:" + JSON.stringify(result))
+        };
+        JPush.addCustomMessagegListener(this.customMessageListener);
+        //tag alias事件回调
+        this.tagAliasListener = result => {
+            console.log("tagAliasListener:" + JSON.stringify(result))
+        };
+        JPush.addTagAliasListener(this.tagAliasListener);
+        //手机号码事件回调
+        this.mobileNumberListener = result => {
+            console.log("mobileNumberListener:" + JSON.stringify(result))
+        };
+        JPush.addMobileNumberListener(this.mobileNumberListener);
+    }
 
     render() {
         return (
             <View style={styles.container}>
-                <Text> MeScreen </Text>
+                   <Button title="setLoggerEnable"
+                        onPress={() => JPush.setLoggerEnable(true)
+                        }/>
+
+                <Button title="getRegisterID"
+                        onPress={() => JPush.getRegistrationID(result =>
+                            console.log("registerID:" + JSON.stringify(result))
+                        )}/>
             </View>
         );
     }
@@ -16,7 +81,24 @@ export default class MeScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
         justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
     },
+    setBtnStyle: {
+        width: 320,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
+        borderWidth: 1,
+        borderColor: '#3e83d7',
+        borderRadius: 8,
+        backgroundColor: '#3e83d7',
+        padding: 10
+    },
+    textStyle: {
+        textAlign: 'center',
+        fontSize: 25,
+        color: '#ffffff'
+    }
 });
