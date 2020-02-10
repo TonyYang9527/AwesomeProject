b885cd930887826720dc26c9d17a1d7dfd90ef96
Tony <yangxiangjiang19880805@gmail.com>
2020-02-11 01:00:09 +0800

    jpush  function merge to master

---
+++ android/app/src/main/java/com/awesomeproject/PushService.java
@@ -0,0 +1,8 @@
+package com.awesomeproject;
+
+//import cn.jpush.android.service.JCommonService;
+
+//public class PushService extends JCommonService {
+public class PushService  {
+
+}
--- src/screens/MeScreen.js
+++ src/screens/MeScreen.js
@@ -1,12 +1,43 @@
 import React ,{Component} from 'react';
-import { StyleSheet,View,Text} from 'react-native';
+import { StyleSheet,View,Text,TouchableHighlight} from 'react-native';
+import JPush from 'jpush-react-native';
+
+
+
+class Button extends React.Component {
+    render() {
+        return <TouchableHighlight
+            onPress={this.props.onPress}
+            underlayColor='#e4083f'
+            activeOpacity={0.5}
+        >
+            <View
+                style={styles.setBtnStyle}>
+                <Text
+                    style={styles.textStyle}>
+                    {this.props.title}
+                </Text>
+            </View>
+        </TouchableHighlight>
+    }
+}
+
 
 export default class MeScreen extends Component {
-
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
             <View style={styles.container}>
-                <Text> MeScreen </Text>
+                   <Button title="setLoggerEnable"
+                        onPress={() => JPush.setLoggerEnable(true)
+                        }/>
+
+                <Button title="getRegisterID"
+                        onPress={() => JPush.getRegistrationID(result =>
+                            console.log("registerID:" + JSON.stringify(result))
+                        )}/>
             </View>
         );
     }
@@ -16,7 +47,24 @@ export default class MeScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
         justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
     },
+    setBtnStyle: {
+        width: 320,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
+        borderWidth: 1,
+        borderColor: '#3e83d7',
+        borderRadius: 8,
+        backgroundColor: '#3e83d7',
+        padding: 10
+    },
+    textStyle: {
+        textAlign: 'center',
+        fontSize: 25,
+        color: '#ffffff'
+    }
 });
